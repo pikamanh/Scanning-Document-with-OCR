a4b08ae51369e1a9c48ee3b3fa63a7c65399d465
pikamanh <hungmanh190204.github@gmail.com>
2025-10-19 22:38:59 +0700

    train YOLO with dataset v1 (not good)

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 from ultralytics import YOLO
 import torch
+import cv2
+import numpy as np
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 def main():
-    model = YOLO(r"runs\pose\train3\weights\best.pt")
-    image = r'dataset\valid\images\JSZZ1394802772779196_jpg.rf.411e87c37bd7003efab6a8b7a6e28092.jpg'
+    model = YOLO(r"model\best v1.pt")
+    image = cv2.imread(r'test.jpg')
+    # image = cv2.imread(r"datasets\ID Card (VN) v1\train\images\00_png_jpg.rf.2cbb14a0813594ec3831406d94bc9732.jpg")
+    image = cv2.resize(image, (640, 640))
+    image_copy = image.copy()
 
-    results = model(image, device=device)
+    results = model.predict(image, device=device, conf=0.8)
 
-    for result in results:
-        xy = result.keypoints.xy
-        xyn = result.keypoints.xyn
-        kpts = result.keypoints.data
+    try:
+        for result in results:
+            kpts = result.keypoints.data.cpu().numpy()
+            print(kpts.shape)
 
-        print(f"xy: {xy}")
-        print(f"xyn: {xyn}")
-        print(f"kpts: {kpts}")
+            for kpt in kpts:
+                for x, y, conf in kpt:
+                    cv2.circle(image_copy, (int (x), int(y)), radius=5, color=(0, 255, 0), thickness=-1)
+
+            # img_result = cv2.drawKeypoints(image=image.copy(), keypoints=keypoints, outImage=0, color=(0, 255, 0))
+            cv2.imshow("test", image_copy)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+    except Exception as e:
+        print(f"Có thể không detect được.")
+        print(e)
 
 if __name__ == "__main__":
     main()
